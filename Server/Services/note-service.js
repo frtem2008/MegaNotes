35bafc2143ca89d6b3991736cb8f464ccb346f5c
frtem2008 <ungazhiv2008@yandex.ru>
2023-06-17 12:23:35 +0300

    Db support

--- Server/Services/note-service.js
+++ Server/Services/note-service.js
@@ -1,17 +1,38 @@
 const { MongoClient, ObjectId } = require("mongodb");
 
-async function connectToDb(dbUrl) {}
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => {
+    console.log(` - ${db.name}`);
+  });
+}
+
+async function connectToDb(noteService, dbUri) {
+  noteService.dbClient = new MongoClient(dbUri);
+  try {
+    await noteService.dbClient.connect();
+    console.log("Db list:\n" + (await listDatabases(noteService.dbClient)));
+  } catch (error) {
+    console.error("Error connecting to database: " + error);
+  } finally {
+    await noteService.dbClient.close();
+  }
+}
 
 class NoteService {
-  constructor(dataDir, dbUrl) {
+  constructor(dataDir, dbUri) {
     this.rootDir = dataDir;
-    this.dbUrl = dbUrl;
+    this.dbUri = dbUri;
+    this.dbClient = undefined;
+    connectToDb(this, this.dbUri);
 
     console.log("Note service created: " + this);
   }
 
   toString() {
-    return `NoteService{"dataDir" : ${this.rootDir}, \n"dbUrl" : ${this.dbUrl}}`;
+    return `NoteService{"dataDir" : ${this.rootDir}, \n"dbUrl" : ${this.dbUri}}`;
   }
 }
 
